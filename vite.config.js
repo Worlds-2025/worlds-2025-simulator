e7754be46b87be2452f9399b11403428ef9424aa
Worlds-2025 <chiangyb@icloud.com>
2025-10-05 21:47:07 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Worlds-2025/', // 替换为你的仓库名
+  base: '/worlds-2025-simulator/', // 替换为你的仓库名
   build: {
     outDir: 'dist',
     sourcemap: false,
